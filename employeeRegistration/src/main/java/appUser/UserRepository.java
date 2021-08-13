10997fb1a569e3c00e7f9f9d728b34f244907b0e
SpaceCowboyAZ <77759985+SpaceCowboyAZ@users.noreply.github.com>
2021-08-13 09:24:25 -0700

    Add files via upload

---
+++ employeeRegistration/src/main/java/appUser/UserRepository.java
@@ -0,0 +1,15 @@
+package appUser;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Repository
+@Transactional(readOnly = true)
+public interface UserRepository {
+	
+	Optional<AppUser> findByEmail(String email);
+
+}
---
+++ employeeRegistration/src/main/java/registration/RegistrationService.java
@@ -0,0 +1,10 @@
+package registration;
+
+public class RegistrationService {
+
+	public String register(registrationRequest request) {
+		// TODO Auto-generated method stub
+		return "works";
+	}
+
+}
