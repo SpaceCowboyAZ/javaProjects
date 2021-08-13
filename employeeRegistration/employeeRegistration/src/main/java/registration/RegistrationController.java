1ebfead14e7360b7a0f2788251c1598efe3c455d
SpaceCowboyAZ <77759985+SpaceCowboyAZ@users.noreply.github.com>
2021-08-13 07:43:39 -0700

    Add files via upload

---
+++ employeeRegistration/employeeRegistration/src/main/java/registration/RegistrationController.java
@@ -0,0 +1,18 @@
+package registration;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+	@RequestMapping(path = "api/v1/registration")
+	@AllArgsConstructor
+	
+	public class RegistrationController {
+		
+	
+	public String register(@RequestBody RegistrationRequest request) {
+		return registrationService.register(request);
+	}
+}
