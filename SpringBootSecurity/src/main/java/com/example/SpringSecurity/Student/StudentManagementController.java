09aa3cac4c50321da2803489615dd01d3fa9c334
SpaceCowboyAZ <dylankharper147@gmail.com>
2021-10-23 10:44:54 -0700

    fixing files

--- SpringBootSecurity/src/main/java/com/example/SpringSecurity/Student/StudentManagementController.java
+++ SpringBootSecurity/src/main/java/com/example/SpringSecurity/Student/StudentManagementController.java
@@ -3,6 +3,7 @@ package com.example.SpringSecurity.Student;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,26 +21,31 @@ new Student (1, "Anakin SkyWalker"),
 new Student (2, "Ahsoka Tanyo"),
 	new Student (3, "Obi Won Kenobi"));
 	
-	@GetMapping
-	public List<Student> getAllStudents() {
 
+	@GetMapping
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")	
+	public List<Student> getAllStudents() {
+System.out.println("getAllStudents");
 		return STUDENTS;
 		
 	}
 	
 	@PostMapping
+	@PreAuthorize("hasAuthority('student:write')")
 	public void registerNewStudent(@RequestBody Student student) {
 //		System.out.println("registerNewStudent");
 		System.out.println(student);
 	}
 	
 	@DeleteMapping(path = "{studentId}")
+	@PreAuthorize("hasAuthority('student:write')")
 	public void deleteStudent(@PathVariable("studentId") Integer studentId) {
 //		System.out.println("deleteStudent");
 		System.out.println(studentId);
 	}
 	
 	@PutMapping(path = "{studentId}")
+	@PreAuthorize("hasAuthority('student:write')")
 	public void updateStudent(@PathVariable("studentId")Integer studentId, @RequestBody Student student) {
 //		System.out.println("updateStudent");
 		System.out.println(String.format("%s %s", studentId, student));
--- SpringBootSecurity/src/main/java/com/example/SpringSecurity/security/ApplicationSecurityConfig.java
+++ SpringBootSecurity/src/main/java/com/example/SpringSecurity/security/ApplicationSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
@@ -14,9 +15,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity 
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 // annotated at class level with @Configuration annotation to enable web securities in our application
 //defined by WebSecurityConfigurer implementations
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter { 
@@ -34,15 +37,18 @@ public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
 @Override //make sure the public class extends WebSecurityConfigurerAdapter or THIS WON'T WORK
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-		.csrf().disable() //TODO
+//		.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())    Sets up csrf. reference*
+//		.and()
+		.csrf().disable() //csrf - Cross Site Request Forgery
 		.authorizeRequests()
 		.antMatchers("/", "index", "/css/*", "/js/*") // allow requests go thorugh root, index, css, and javascript
-		.permitAll()  //permits patterns above
+		.permitAll()  
 		.antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name()) //** selects all
-		.antMatchers(HttpMethod.DELETE, "management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.name())
-		.antMatchers(HttpMethod.POST, "management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.name())
+		//.antMatchers("management/api/**").hasAnyRole(ApplicationUserRole.ADMIN.name(), ApplicationUserRole.ADMINTRAINEE.name()) //do not have to include GET because it will go through the matchers 1 by 1. 
+	.antMatchers(HttpMethod.DELETE, "management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.name())
+	.antMatchers(HttpMethod.POST, "management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.name())		//kept for old reference
 		.antMatchers(HttpMethod.PUT, "management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.name())
-		.antMatchers(HttpMethod.GET, "management/api/**").hasAnyRole(ApplicationUserRole.ADMIN.name(), ApplicationUserRole.ADMINTRAINEE.name())
+		.antMatchers(HttpMethod.GET, "/api/**").hasRole(ApplicationUserRole.STUDENT.name())
 		.anyRequest()
 		.authenticated()
 		.and()
@@ -65,6 +71,7 @@ protected UserDetailsService userDetailsService() {  //UserDetailsService retrie
 			.username("Obi Won Kenobi")
 			.password(passwordEncoder.encode("password123"))
 			//.roles(ApplicationUserRole.ADMIN.name()) //ROLE_ADMIN
+			.authorities(ApplicationUserRole.ADMIN.getGrantedAuthorities())
 			.build();
 			
 			
@@ -72,6 +79,7 @@ protected UserDetailsService userDetailsService() {  //UserDetailsService retrie
 					.username("Ahsoka Tanyo")
 					.password(passwordEncoder.encode("password123"))
 					//.roles(ApplicationUserRole.ADMINTRAINEE.name()) //ROLE_ADNINTRAINEE
+					.authorities(ApplicationUserRole.ADMINTRAINEE.getGrantedAuthorities())
 					.build();
 					return new InMemoryUserDetailsManager(AnakinSkyWalkerUser, obiUser, ahsokaUser);
 }
